172cc3dfd839ddbd2e91629b9110ebd2578aa321
RovinKYK <kykrovin@gmail.com>
2025-07-09 11:35:28 +0530

    changed config

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
 window.configs = {
-    apiUrl: '/choreo-apis/food-connect/foodconnect-backend-svc/v1/api',
+    apiUrl: 'https://7fdccf0a-20e6-4765-84b1-8e361dd40d1f-dev.e1-us-east-azure.choreoapis.dev/food-connect/foodconnect-backend-svc/v1.0/api',
 }
